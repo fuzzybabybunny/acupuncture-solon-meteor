6cd06db6a8bb261593bfba230bd2575d0d7b4621
fuzzybabybunny <victor@victorlinphoto.com>
2014-11-08 16:51:42 +0800

    added meta to head

--- client/templates/includes/crew.js
+++ client/templates/includes/crew.js
@@ -2,6 +2,10 @@ Template.crew.events({
 
 	'click #ZPModalButton': function(){
 		$('#ZPModal').modal('toggle');
+	},
+
+	'click #ZPModalLink': function(){
+		$('#ZPModal').modal('toggle');
 	}
 
 });

dcdea8a59dbad071d19abeb5bd32643037f9e7c4
fuzzybabybunny <victor@victorlinphoto.com>
2015-01-02 15:22:10 -0500

    got SVG location click to work

--- client/templates/admin/svg_test.js
+++ client/templates/admin/svg_test.js
@@ -6,5 +6,27 @@ Template.SVGTest.rendered = function(){
       this.style.fill = "#"+colors[i++%colors.length];
   });
 
+	var  markerPositions = [[225,175], [75,75], [150,225], [400,125], [300,300]];
+
+	var svgNS = "http://www.w3.org/2000/svg";
+	var xlinkNS = "http://www.w3.org/1999/xlink";
+
+	for (var i=0; i<markerPositions.length; i++) {
+	    // Create an SVG <use> element
+	    var  use = document.createElementNS(svgNS, "use");
+	    // Point it at our pin marker (the circle)
+	    use.setAttributeNS(xlinkNS, "href", "#pin");
+	    // Set it's x and y
+	    use.setAttribute("x", markerPositions[i][0]);
+	    use.setAttribute("y", markerPositions[i][1]);
+	    debugger
+	    use.onclick = function(){
+	    	console.log("clicked!");
+	    };
+
+	    // Add it to the "markers" group
+	    document.getElementById("markers").appendChild(use);
+	    debugger
+	}
 
 };

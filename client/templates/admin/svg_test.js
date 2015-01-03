28ab0acfca861e2f670817931733ddc09a171e6d
fuzzybabybunny <victor@victorlinphoto.com>
2015-01-03 06:59:28 -0500

    got specific pin click and touch to work

--- client/templates/admin/svg_test.js
+++ client/templates/admin/svg_test.js
@@ -14,32 +14,28 @@ Template.SVGTest.rendered = function(){
 		var panZoomArm = svgPanZoom('#map');
 	});
 
-	// $('#map').load(function(){
-	// 	var panZoomMap = svgPanZoom('#map');
-	// });
-
-	var colors = "0f0 0ff f60 f0f 00f f00".split(' '), i=0;
-
-  $('path').click(function(){
-      this.style.fill = "#"+colors[i++%colors.length];
-  });
-
 	var  markerPositions = [[225,175], [75,75], [150,225], [400,125], [300,300]];
 
 	var svgNS = "http://www.w3.org/2000/svg";
 	var xlinkNS = "http://www.w3.org/1999/xlink";
 
+	var alertClicked = function(event){
+		var dataDescription = event.currentTarget.attributes[1].textContent;
+		alert('clicked on ' + dataDescription);
+	};
+
 	for (var i=0; i<markerPositions.length; i++) {
 	    // Create an SVG <use> element
 	    var  use = document.createElementNS(svgNS, "use");
-	    // Point it at our pin marker (the circle)
+	    // use it at our pin marker (the circle)
 	    use.setAttributeNS(xlinkNS, "href", "#pin");
+	    // Set pin's data attribute
+	    use.setAttribute("data-description", "Pin " + i );
 	    // Set it's x and y
 	    use.setAttribute("x", markerPositions[i][0]);
 	    use.setAttribute("y", markerPositions[i][1]);
-	    use.onclick = function(){
-	    	alert("clicked!");
-	    };
+	    use.addEventListener('touchstart', alertClicked, false);
+	    use.addEventListener('click', alertClicked, false);
 
 	    // Add it to the "markers" group
 	    document.getElementById("markers").appendChild(use);

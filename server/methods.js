58b2819a953d336ad94327f64c41858785a337f8
fuzzybabybunny <victor@victorlinphoto.com>
2015-05-28 00:57:08 -0700

    fixed typo

--- server/methods.js
+++ server/methods.js
@@ -6,21 +6,28 @@ Meteor.methods({
 	},
 
 	sendContactEmail: function(contents){
+
 		check(contents, Schema.contact);
+
 		// console.log(contents);
+
 		if (!contents.doctorName){
 			contents.doctorName = '';
 		};
+
 		if (!contents.specificReferral){
 			contents.specificReferral = '';
 		};
+
 		contents.message = contents.message.replace(/\r?\n/g, '<br />');
+
     return Meteor.Mandrill.send({
       to: 'admin@acupuncturecleveland.com',
       from: contents.email,
       subject: contents.subject,
-      html: 'A new message has been sent by ' + contents.name + ' and they found us through: ' + contents.referral + ' ' + contents.doctorName + ' ' + contents.specificReferral + '<br /><br />' + contents.message
+      html: 'A new message has been sent by ' + contents.name + ' and they found us through: ' + contents.referral + ' ' + contents.doctorName + ' ' + contents.specificReferral + '<br /><br />Here is the original message: <br /><br />' + contents.message
     });
+
 	}
 
 
